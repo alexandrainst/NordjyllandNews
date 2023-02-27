b65f4856225aa47c3d9666cf95f4d09aac8f7e1e
Oliver Kinch <oliver.kinch@gmail.com>
2023-02-27 12:47:19 +0100

    Underscores instead of hyphens in cfg

--- src/scripts/build_summarization_dataset.py
+++ src/scripts/build_summarization_dataset.py
@@ -4,19 +4,15 @@ Usage:
     >>> python src/scripts/build_summarization_dataset.py
 """
 
-import logging
-
 import hydra
 from omegaconf import DictConfig
 
 from nordjylland_news.summary_dataset import SummaryDataSetBuilder
 
-logger = logging.getLogger(__name__)
-
 
 @hydra.main(config_path="../../config", config_name="config.yaml")
 def main(cfg: DictConfig) -> None:
-    dataset_name = cfg["data-set-names"]["summary_dataset"]
+    dataset_name = cfg["data_set_names"]["summary_dataset"]
     builder = SummaryDataSetBuilder(dataset_name=dataset_name, cfg=cfg)
     builder.build_dataset()
 
