558c6e8bd5c9960fac21945b2494afd05c5134eb
Oliver Kinch <oliver.kinch@gmail.com>
2023-02-27 12:42:40 +0100

    Sleeps from constants.py to config.yaml

--- src/nordjylland_news/constants.py
+++ src/nordjylland_news/constants.py
@@ -7,8 +7,3 @@ HEADERS = {"Accept": "application/json", "Authorization": "centered"}
 STATUS_CODE_OK = 200
 TOO_MANY_REQUESTS = 429
 ERROR_500 = 500
-
-# Sleep times
-SLEEP_SHORT = 5  # 5 seconds
-SLEEP_MEDIUM = 60  # 1 minute
-SLEEP_LONG = 900  # 15 minutes
