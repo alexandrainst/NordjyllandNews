a5a79921a6128461a260b08652fb6c4197abf42b
Oliver Kinch <oliver.kinch@gmail.com>
2023-09-08 08:51:11 +0200

    Upload image captioning dataset to Hugging Face

---
+++ src/scripts/upload_image_captioning.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+
+DATASET_PATH = (
+    "../../../../../mnt/data_6tb/oliver/NordjyllandNews/data/processed/images/train"
+)
+DATASET_HF_PATH = "oliverkinch/nordjylland-news-image-captioning"
+
+dataset = load_dataset("imagefolder", data_dir=DATASET_PATH, split="train")
+dataset.push_to_hub(DATASET_HF_PATH, private=True)
