586e322053e6bc654c8761a0c288367354b60dad
Oliver Kinch <oliver.kinch@gmail.com>
2023-09-19 15:11:37 +0200

    Push to alexaindrainst

--- src/scripts/upload_image_captioning.py
+++ src/scripts/upload_image_captioning.py
@@ -10,7 +10,7 @@ from datasets import load_dataset
 DATASET_PATH = (
     "../../../../../mnt/data_6tb/oliver/NordjyllandNews/data/processed/images/train"
 )
-DATASET_HF_PATH = "oliverkinch/nordjylland-news-image-captioning"
+DATASET_HF_PATH = "alexandrainst/nordjylland-news-image-captioning"
 
 dataset = load_dataset("imagefolder", data_dir=DATASET_PATH, split="train")
 dataset.push_to_hub(DATASET_HF_PATH, private=True)
