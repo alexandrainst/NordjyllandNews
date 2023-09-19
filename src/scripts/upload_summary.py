7112d5646900241955840b46eb1a9c3bab6b9430
Oliver Kinch <oliver.kinch@gmail.com>
2023-09-19 15:09:02 +0200

    Bug fixed
    For some reason every sample was included twice. But the script now
    behaves as expected

--- src/scripts/upload_summary.py
+++ src/scripts/upload_summary.py
@@ -7,11 +7,25 @@ Usage:
 
 from datasets import load_dataset
 
+# Number of samples for each split are from notebooks/process_summary_data.ipynb
+TRAIN_LENGTH = 75219
+VAL_LENGTH = 4178
+TEST_LENGTH = 4178
+
 DATASET_FOLDER_PATH = (
     "../../../../../mnt/data_6tb/oliver/NordjyllandNews/data/processed/summary"
 )
-DATASET_HF_PATH = "oliverkinch/nordjylland-news-summarization"
+DATASET_HF_PATH = "alexandrainst/nordjylland-news-summarization"
 
+data_files = dict(
+    train=DATASET_FOLDER_PATH + "/train.jsonl",
+    val=DATASET_FOLDER_PATH + "/val.jsonl",
+    test=DATASET_FOLDER_PATH + "/test.jsonl",
+)
+dataset = load_dataset("json", data_files=data_files)
 
-dataset = load_dataset(DATASET_FOLDER_PATH)
-dataset.push_to_hub(DATASET_HF_PATH, private=True)
+assert len(dataset["train"]) == 75219
+assert len(dataset["val"]) == 4178
+assert len(dataset["test"]) == 4178
+
+dataset.push_to_hub(DATASET_HF_PATH, private=False)
